60de99e8b1fa3833bd192c61cd88ae0e478f832a
Yahia Hassan <yehia4747@gmail.com>
2018-06-28 00:37:57 +0200

    Add Jokes to the Java library

--- joketeller/src/main/java/com/example/joketeller/JokeProvider.java
+++ joketeller/src/main/java/com/example/joketeller/JokeProvider.java
@@ -1,7 +1,18 @@
 package com.example.joketeller;
 
+import java.util.Random;
+
 public class JokeProvider {
+
+    private final String[] jokes = new String[]{
+            "Q: What's the object-oriented way to become wealthy? \n A: Inheritance",
+            "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.",
+            "What happens to a frog's car when it breaks down? \nIt gets toad away.",
+            "If you ever get cold, stand in the corner of a room for a while. \nThey're usually 90 degrees.",
+            "What's the best thing about Switzerland? \nI don't know, but the flag is a big plus."
+    };
+
     public String getJoke() {
-        return "Funny Joke";
+        return jokes[new Random().nextInt(jokes.length)];
     }
 }
